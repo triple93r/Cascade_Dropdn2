d447cd1da5371fd0b2623729e2f2e6ad61c811dc
triple93r <56282248+triple93r@users.noreply.github.com>
2023-11-21 08:19:46 +0530

    init

---
+++ Cascd_Dpdwn/Cascd_Dpdwn/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cascd_Dpdwn.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public virtual DbSet<tblBananaStock> TblBananaStock { get; set; }
+    }
+}
