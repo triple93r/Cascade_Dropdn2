dd98a1c301ed096a075f6dc68987277f6878dc9a
triple93r <56282248+triple93r@users.noreply.github.com>
2023-11-22 07:54:02 +0530

    chng3

--- Cascd_Dpdwn/Cascd_Dpdwn/Controllers/HomeController.cs
+++ Cascd_Dpdwn/Cascd_Dpdwn/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Cascd_Dpdwn.Models;
+﻿using Cascd_Dpdwn.Data;
+using Cascd_Dpdwn.Models;
+using Cascd_Dpdwn.VMs;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Cascd_Dpdwn.Controllers
@@ -7,10 +10,11 @@ namespace Cascd_Dpdwn.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -25,6 +29,9 @@ namespace Cascd_Dpdwn.Controllers
 
         public IActionResult DisplayData()
         {
+            AllTbl allTbl = new AllTbl();
+            allTbl.Territory = _context.tbl_Territory.ToList();
+            
             return View();
         }
 
---
+++ Cascd_Dpdwn/Cascd_Dpdwn/VMs/AllTbl.cs
@@ -0,0 +1,10 @@
+﻿using Cascd_Dpdwn.Models;
+
+namespace Cascd_Dpdwn.VMs
+{
+    public class AllTbl
+    {
+        public tbl_Territory Territory { get; set; }
+
+    }
+}
